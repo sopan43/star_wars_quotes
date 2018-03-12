03b70f4fd1789573ec39ac51bd3c4aae209a7e8f
Sopan Mittal <sopanmittal43@gmail.com>
2018-03-12 18:40:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+var db
+
+//mongodb://<admin>:<admin@1234>@ds125126.mlab.com:25126/sopan-starwars
+
+MongoClient.connect('mongodb://admin12:admin12345@ds125126.mlab.com:25126/sopan-starwars', (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('sopan-starwars') // whatever your database name is
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+})
+
+
+
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray(function(err, results) {
+        console.log(results)
+    })
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
+})
