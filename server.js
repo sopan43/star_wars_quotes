ff0e7ffb948951bbb12c78cff4fae4070b423f59
Sopan Mittal <sopanmittal43@gmail.com>
2018-03-14 22:48:23 +0530

    bug fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
